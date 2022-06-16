568b1a9c0dc73b6eee7ce60ab04f698247e7389d
Kenan Kadic <ken.kadic@outlook.com>
2022-06-16 09:03:48 -0500

    winner class added

--- script.js
+++ script.js
@@ -8,8 +8,10 @@
 let player0El = document.querySelector('.player--0')
 let player1El = document.querySelector('.player--1')
 
-let score0El = document.querySelector('#score--0');
-let score1El = document.querySelector('#score--1');
+//querySelector needs # if it is an id
+
+let totalScore0El = document.querySelector('#score--0');
+let totalScore1El = document.querySelector('#score--1');
 let diceEl = document.querySelector('.dice');
 let btnNew = document.querySelector('.btn--new');
 let btnRoll = document.querySelector('.btn--roll');
@@ -22,12 +24,20 @@ let current1El = document.querySelector('#current--1');
 const scores = [0, 0];
 let activePlayer = 0;
 
+const switchPlayer = function () {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
 // starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
+totalScore0El.textContent = 0;
+totalScore1El.textContent = 0;
 diceEl.classList.add('hidden')
 
-// ROll dice functionality
+// Roll dice functionality
 
 btnRoll.addEventListener('click', function (){
     //1. Generate random dice roll
@@ -41,15 +51,28 @@ btnRoll.addEventListener('click', function (){
         currentScore+=dice;
         document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
     } else {
-        document.querySelector(`#current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
 
-        //toggle adds class if not there--if there removes it
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer()
     }
-    //If true switch to next player
+})
+btnHold.addEventListener('click', function (){
+    // add current score to activePlayer score
+
+    //when it is player zero it will be scores[0]
+    //when it is player one it will be scores[1]
+    //scores means total score really - total for player plus current gives you new score
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // if score is over 100
+    if ((scores[activePlayer]) >= 100) {
+        // finish the game
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    }
+
+     //switch player
+    switchPlayer()
 })
 
 
